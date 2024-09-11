0191b7cb597e103023c9ab81bb3ab51bfd9a4e2a
YashKhunteta <101105245+YashKhunteta@users.noreply.github.com>
2024-09-11 12:33:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+
 nltk.download('punkt')
 nltk.download('stopwords')
 
